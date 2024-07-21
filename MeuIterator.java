a79b73243460c698dc689a5b04411309c3227a8c
tiagolcxt <165700502+tiagolcxt@users.noreply.github.com>
2024-07-21 13:19:35 -0300

    Delete MeuIterator.java

--- MeuIterator.java
+++
@@ -1,9 +0,0 @@
-public interface MeuIterator<T>{
-    
-    /*criação de uma interface genérica com métodos abstratos a serem implementados, 
-    Onde T delimita o tipo de coleção*/
-
-    boolean hasNext();
-    T next();
-
-}
